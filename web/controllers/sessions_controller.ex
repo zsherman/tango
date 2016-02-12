0db7e8b8b995b71143288414305709781126ca6e
Zach Sherman <zksherm@gmail.com>
2016-02-12 18:25:20 -0500

    udpates

--- web/controllers/sessions_controller.ex
+++ web/controllers/sessions_controller.ex
@@ -1,15 +1,34 @@
 defmodule Tango.SessionsController do
-  use Tango.Web, :controller
+  require Logger
 
-  def index(conn, _params) do
+  use Tango.Web, :controller
+  alias Tango.Session
+  alias Tango.Router
+
+  def index(conn) do
     render conn, "index.html"
   end
 
   def create(conn, _params) do
-    
+    length = 10
+    unique_id = :crypto.strong_rand_bytes(length) |> Base.url_encode64 |> binary_part(0, length)
+
+    changeset = Session.changeset(%Session{},
+      %{slug: unique_id}
+    )
+
+    Session.insert!(changeset)
+
+    # redirect conn, to: Router.Helpers.sessions_path(conn, :show, session)
+    redirect conn, to: '/sessions/#{unique_id}'
   end
 
   def show(conn, %{"session" => session} = params) do
     render conn, "show.html", session: session
+  end
+
+  # generate random string
+  def random_string(length) do
+    :crypto.strong_rand_bytes(length) |> Base.url_encode64 |> binary_part(0, length)
   end
 end
--- web/views/session_view.ex
+++ web/views/session_view.ex
@@ -1,3 +1,7 @@
 defmodule Tango.SessionsView do
   use Tango.Web, :view
+
+  def csrf_token(conn) do
+    Plug.Conn.get_session(conn, :csrf_token)
+  end
 end
